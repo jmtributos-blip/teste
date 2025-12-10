5245ed4f85d90ae0d3e4ff04314a699b87f688e4
Tan <jmtributos@gmail.com>
2025-12-10 10:55:20 -0300

    Update app_viewer.py

--- app_viewer.py
+++ app_viewer.py
@@ -727,8 +727,8 @@ else:
 
     # Ordena as competências de maneira decrescente
     if 'Competência' in df_full.columns:
-    available_competencias = df_full['Competência'].unique().tolist()
-    available_competencias = sorted(available_competencias)
+        available_competencias = df_full['Competência'].unique().tolist()
+        available_competencias = sorted(available_competencias)
 else:
     available_competencias = []
     st.warning("A coluna 'Competência' não foi encontrada no DataFrame.")
@@ -1255,6 +1255,7 @@ Base.metadata.create_all(engine)
 # Configurar conexão com o banco e criar uma sessão para adicionar/registros
 Session = sessionmaker(bind=engine)
 session = Session()
+
 
 
 
